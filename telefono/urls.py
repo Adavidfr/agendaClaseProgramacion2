04541af8108046128c42858df12244501479f860
Adavidfr <adavidfruiz@gmail.com>
2025-04-16 12:11:57 -0500

    feat: expand telefono URL patterns to include contact management routes

--- telefono/urls.py
+++ telefono/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import hola_mundo
+from .views import *
 
 urlpatterns = [
-    path('', hola_mundo, name="hola_mundo")
+    path('hola/', hola_mundo, name="hola_mundo"),
+    path('', lista_contactos, name= 'lista_contactos'),
+    path('agregar', agregar_contactos, name='agregar_contactos'),
+    path('editar/<int:id>', editar_contacto, name='editar_contacto'),
+    path('eliminar/<int:id>', eliminar_contacto, name='eliminar_contacto'),
 ]

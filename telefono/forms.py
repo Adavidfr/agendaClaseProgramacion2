326ff62b078c4184f9884707c2914e6a0ba01a9c
Adavidfr <adavidfruiz@gmail.com>
2025-04-28 11:10:50 -0500

    feat: update custom user creation form to use User model instead of UserCreationForm

--- telefono/forms.py
+++ telefono/forms.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django import forms
 from .models import Contacto
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
-        model = UserCreationForm
+        model = User
         fields = ['username', 'email', 'password1', 'password2']
         
 class ContactoForm(forms.ModelForm):
